3c45cc9a5e7499706791362f01330cbf842eca1b
Muhammad Hasbi Muharyadi <hasbi.muharyadi@gmail.com>
2017-06-01 11:53:41 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var scene, camera, renderer;
+
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var SPEED = 0.01;
+
+function init() {
+    scene = new THREE.Scene();
+
+    initMesh();
+    initCamera();
+    initLights();
+    initRenderer();
+
+    document.body.appendChild(renderer.domElement);
+}
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 3.5, 5);
+    camera.lookAt(scene.position);
+}
+
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initLights() {
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
+
+var mesh = null;
+function initMesh() {
+    var loader = new THREE.JSONLoader();
+    loader.load('fox.js', function(geometry, materials) {
+        mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+        mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
+        mesh.translation = THREE.GeometryUtils.center(geometry);
+        scene.add(mesh);
+    });
+}
+
+function rotateMesh() {
+    if (!mesh) {
+        return;
+    }
+
+    mesh.rotation.x = 0;
+    mesh.rotation.y = 1.5;
+    mesh.rotation.z = 0;
+}
+
+function render() {
+    requestAnimationFrame(render);
+    rotateMesh();
+    renderer.render(scene, camera);
+}
+
+init();
+render();
